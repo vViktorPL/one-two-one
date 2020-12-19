f45e17fd929af357d7d8d3c007a6bb4702013ab4
Wiktor Toporek <wiktor@tsh.io>
2020-12-20 00:08:43 +0100

    add level 0

--- src/Screen/Game/Level/Index.elm
+++ src/Screen/Game/Level/Index.elm
@@ -1,6 +1,7 @@
 module Screen.Game.Level.Index exposing (firstLevel, restLevels)
 
 import Screen.Game.Level exposing (Level)
+import Screen.Game.Level.Level0
 import Screen.Game.Level.Level1
 import Screen.Game.Level.Level2
 import Screen.Game.Level.Level3
@@ -9,7 +10,7 @@ import Screen.Game.Level.Level5
 
 
 firstLevel : Level
-firstLevel = Screen.Game.Level.Level1.data
+firstLevel = Screen.Game.Level.Level0.data
 
 restLevels : List Level
-restLevels = [Screen.Game.Level.Level2.data, Screen.Game.Level.Level3.data, Screen.Game.Level.Level4.data, Screen.Game.Level.Level5.data]
+restLevels = [Screen.Game.Level.Level1.data, Screen.Game.Level.Level2.data, Screen.Game.Level.Level3.data, Screen.Game.Level.Level4.data, Screen.Game.Level.Level5.data]
