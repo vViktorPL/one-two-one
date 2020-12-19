7000c0475f04e3c3a422b5e5a51a2214a59dbca8
Wiktor Toporek <wiktor@tsh.io>
2020-12-19 23:59:15 +0100

    add 2 rusty levels

--- src/Screen/Game/Level/Index.elm
+++ src/Screen/Game/Level/Index.elm
@@ -4,10 +4,12 @@ import Screen.Game.Level exposing (Level)
 import Screen.Game.Level.Level1
 import Screen.Game.Level.Level2
 import Screen.Game.Level.Level3
+import Screen.Game.Level.Level4
+import Screen.Game.Level.Level5
 
 
 firstLevel : Level
 firstLevel = Screen.Game.Level.Level1.data
 
 restLevels : List Level
-restLevels = [Screen.Game.Level.Level2.data, Screen.Game.Level.Level3.data]
+restLevels = [Screen.Game.Level.Level2.data, Screen.Game.Level.Level3.data, Screen.Game.Level.Level4.data, Screen.Game.Level.Level5.data]
--- src/Screen/Game/Level/Level1.elm
+++ src/Screen/Game/Level/Level1.elm
@@ -2,4 +2,4 @@ module Screen.Game.Level.Level1 exposing (data)
 import Screen.Game.Level exposing (Level, LevelTile(..), fromData)
 
 data : Level
-data = fromData [[Floor,Floor,Floor],[Floor,Floor,Floor,Floor,RustyFloor,RustyFloor],[Floor,Floor,Floor,Floor,Floor,Floor,Floor,Floor,Floor],[Empty,Floor,Floor,Floor,Floor,Floor,Floor,Floor,Floor,Floor],[Empty,Empty,Empty,Empty,Empty,Floor,Floor,Finish,Floor,Floor],[Empty,Empty,Empty,Empty,Empty,Empty,Floor,Floor,Floor],[]] (1, 1)
+data = fromData [[Floor,Floor,Floor],[Floor,Floor,Floor,Floor,Floor,Floor],[Floor,Floor,Floor,Floor,Floor,Floor,Floor,Floor,Floor],[Empty,Floor,Floor,Floor,Floor,Floor,Floor,Floor,Floor,Floor],[Empty,Empty,Empty,Empty,Empty,Floor,Floor,Finish,Floor,Floor],[Empty,Empty,Empty,Empty,Empty,Empty,Floor,Floor,Floor],[]] (1, 1)
