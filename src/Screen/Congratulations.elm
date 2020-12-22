16ab99c113e9bf23033721995bfed7b33ce0d942
Wiktor Toporek <wiktor@tsh.io>
2020-12-22 19:34:16 +0100

    Congratulations screen small polishing

--- src/Screen/Congratulations.elm
+++ src/Screen/Congratulations.elm
@@ -1,6 +1,7 @@
 module Screen.Congratulations exposing (Congratulations, init, view)
 
 import Html exposing (Html)
+import Html.Attributes as Attr
 
 
 type alias Congratulations =
@@ -14,4 +15,4 @@ init =
 
 view : Congratulations -> Html msg
 view _ =
-    Html.div [] [ Html.text "Congratulations!" ]
+    Html.div [ Attr.style "font-size" "10vw", Attr.style "text-align" "center", Attr.style "color" "black" ] [ Html.text "Congratulations!" ]
